ca8fbff5c628949ebf246ae52f3b965a4ca8b2b8
Gustavo Salgado <gsalgadotoledo@gmail.com>
2014-11-03 18:32:38 -0500

    Fixed on radio input generator

--- modules/gii/generators/input/templates/radio/input.php
+++ modules/gii/generators/input/templates/radio/input.php
@@ -10,14 +10,18 @@ $viewName=basename($this->viewName);
 ?>
 <div class="form-group">
     <?php echo "<?php echo \$form->labelEx(\$model,'{$this->viewName}',array('class'=>'control-label')); ?>\n"?>
-    <?php echo "<?php echo \$this->widget('ext.inputs.colorpicker.EColorPicker', array(
+    <?php echo "<?php echo \$this->widget('ext.inputs.radio.GThumbnail', array(
                 'model'=>\$model,
                 'attribute'=>'{$this->viewName}',
                 'htmlOptions'=>array('class'=>'form-control'),
-                'mode'=>'textfield',
-                'fade' => false,
-                'slide' => false,
-                'curtain' => true,
-           ),true)?>\n"?>
+                'listData'=>array(
+                  '1'=>'Example <strong>html</strong> support 1',
+                  '2'=>'Example <strong>html</strong> support 2',
+                  '3'=>'Example <strong>html</strong> support 3',
+                  '4'=>'Example <strong>html</strong> support 4',
+                  '5'=>'Example <strong>html</strong> support 5',
+                  '6'=>'Example <strong>html</strong> support 6'
+                ),
+            ),true)?>\n"?>
     <?php echo "<?php echo \$form->error(\$model,'{$this->viewName}',array('class'=>'help-block')); ?>\n"?>
 </div>
