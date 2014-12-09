a561753ff7184301b661f88b736fa741b8756230
Gustavo Salgado <gsalgadotoledo@gmail.com>
2014-12-09 18:32:31 -0500

    Last changes

--- modules/users/views/page/index.php
+++ modules/users/views/page/index.php
@@ -10,27 +10,38 @@ $this->pageTitle=Yii::app()->name . ' - '.Yii::t('app','Profile');
     <div class="container">
         <div class="row row-no-padding">
             
-            <div class="col-md-4">
-                <img src="<?=$model->imageUrl?>" alt="">
-            </div>
-            <div class="col-md-8">
+            <div class="col-md-3">
+                <img class="img-thumbnail" src="<?=$model->imageUrl?>" alt="">
 
+                <?=$model->email?><br>
+            </div>
+            <div class="col-md-9">
+                <h1><?=$model->name?> <?=$model->lastname?></h1>
+                <a href="<?=$this->createUrl("perfil")?>">Editar perfil</a>
                 <div role="tabpanel">
 
                   <!-- Nav tabs -->
                   <ul class="nav nav-tabs" role="tablist">
-                    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-                    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-                    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-                    <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+                    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Novedades (1)</a></li>
+                    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Mis periódicos</a></li>
+                    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Suscriptores</a></li>
+                    <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Mis suscripciones</a></li>
                   </ul>
 
                   <!-- Tab panes -->
                   <div class="tab-content">
-                    <div role="tabpanel" class="tab-pane active" id="home">...</div>
-                    <div role="tabpanel" class="tab-pane" id="profile">...</div>
-                    <div role="tabpanel" class="tab-pane" id="messages">...</div>
-                    <div role="tabpanel" class="tab-pane" id="settings">...</div>
+                    <div role="tabpanel" class="tab-pane active" id="home">
+
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="profile">
+
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="messages">
+
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="settings">
+
+                    </div>
                   </div>
 
                 </div>
