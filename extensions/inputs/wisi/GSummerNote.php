012ac88262828d3f9533926a3c45272f36d506e4
Gustavo Salgado <gsalgadotoledo@gmail.com>
2014-12-11 14:57:50 -0500

    Add summernote wisiwing

---
+++ extensions/inputs/wisi/GSummerNote.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Redactor widget
+ *
+ * @author Jani Mikkonen <janisto@php.net>
+ * @version 1.1
+ * @license public domain (http://unlicense.org)
+ * @package extensions.redactor
+ * @link http://imperavi.com/redactor/
+ */
+
+class GSummerNote extends CInputWidget {
+	/**
+	 * Assets package ID.
+	 */
+	const PACKAGE_ID = 'redactor-widget';
+
+	/**
+	 * @var string path to assets
+	 */
+	protected $assetsPath;
+
+	/**
+	 * @var string URL to assets
+	 */
+	protected $assetsUrl;
+
+	/**
+	 * @var array redactor options
+	 * @see http://redactorjs.com/docs
+	 */
+	public $options = array();
+
+	/**
+	 * @var string|null textarea selector for jQuery
+	 */
+	public $selector;
+
+	/**
+	 * Init widget
+	 */
+	public function init()
+	{
+		parent::init();
+		if ($this->assetsPath === null) {
+			$this->assetsPath = dirname(__FILE__).DIRECTORY_SEPARATOR.'assets';
+		}
+		if ($this->assetsUrl === null) {
+			$this->assetsUrl = Yii::app()->assetManager->publish($this->assetsPath);
+		}
+		if ($this->selector === null) {
+			list($this->name, $this->id) = $this->resolveNameId();
+			$this->selector = '#' . $this->id;
+		}
+		$this->registerClientScript();
+	}
+
+	/**
+	 * Run widget.
+	 */
+	public function run()
+	{
+		if ($this->hasModel()) {
+			echo CHtml::activeTextArea($this->model, $this->attribute, $this->htmlOptions);
+		} else if ($this->selector !== null) {
+			echo CHtml::textArea($this->name, $this->value, $this->htmlOptions);
+		}
+	}
+
+	/**
+	 * Register CSS and scripts.
+	 */
+	protected function registerClientScript()
+	{
+		$cs = Yii::app()->clientScript;
+		$cs->registerScriptFile($this->assetsUrl.'/dist/summernote.min.js');
+		$cs->registerCssFile($this->assetsUrl.'/dist/summernote.css');
+		$cs->registerScript(
+			__CLASS__ . '#' . $this->id,
+			'jQuery('. CJavaScript::encode($this->selector) .').summernote('. CJavaScript::encode($this->options) .');',
+			CClientScript::POS_READY
+		);
+	}
+}
