9494c735484fec5c7207c3ff8bd6fb5d04315656
Gustavo Salgado <gsalgadotoledo@gmail.com>
2014-12-27 19:45:39 -0500

    Add inifinite scroll widget error

--- extensions/widgets/yiinfinite-scroll/YiinfiniteScroller.php
+++ extensions/widgets/yiinfinite-scroll/YiinfiniteScroller.php
@@ -66,7 +66,7 @@ class YiinfiniteScroller extends CBasePager {
     }
 
     public function registerClientScript() {
-        $url = CHtml::asset(Yii::getPathOfAlias('ext.yiinfinite-scroll.assets').'/jquery.infinitescroll.min.js');
+        $url = CHtml::asset(Yii::getPathOfAlias('ext.widgets.yiinfinite-scroll.assets').'/jquery.infinitescroll.min.js');
         Yii::app()->clientScript->registerScriptFile($url);
     }
 
