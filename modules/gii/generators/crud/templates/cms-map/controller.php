19403ba5162023e30312dcd77c06c851dc131285
Gustavo Salgado <gsalgadotoledo@gmail.com>
2014-12-29 12:02:31 -0500

    New map generator on testing

---
+++ modules/gii/generators/crud/templates/cms-map/controller.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * This is the template for generating a controller class file for CRUD feature.
+ * The following variables are available in this template:
+ * - $this: the CrudCode object
+ */
+?>
+<?php echo "<?php\n"; ?>
+
+class <?php echo $this->controllerClass; ?> extends CmsController
+{
+	/////////////////////////////
+	// This controller is for  //
+	// Back actions            //
+	/////////////////////////////
+	
+	public $defaultAction='admin';
+<?php if($this->fontIcon!==null):?>
+	public $title='<i class="fa <?php echo $this->fontIcon?>"></i> <?php echo $this->labelName; ?>';
+<?php else:?>
+	public $title='<?php echo $this->labelName; ?>';
+<?php endif;?>
+	public $subTitle='Admin <?php echo $this->labelName; ?>';
+	
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array('index'),
+				'roles'=>$this->module->getAllowPermissoms(),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}
+
+
+	public function actionIndex()
+	{
+		$model=new <?php echo $this->modelClass; ?>('search');
+		$model->unsetAttributes();  // clear any default values
+		if(isset($_GET['<?php echo $this->modelClass; ?>']))
+		{
+			$model->attributes=$_GET['<?php echo $this->modelClass; ?>'];
+			// Nombres de filtros que llegan desde
+			// el formulario de búsqueda
+		}
+		
+		$list=<?php echo $this->modelClass; ?>::model()->findAll($model->search()->getCriteria());
+		// $list=<?php echo $this->modelClass; ?>::model()->findAll($model->searchMap()->getCriteria());
+		
+		$models = array();
+		foreach($list as $data)
+		{
+			$attr=$data->attributes;
+		    // $attr['color']=$data->getColorPin();
+		    // $attr['description_service']=$data->getDescriptionPin();
+		    // $attr['content_event']="
+			// <div class=\"row row-map\">
+			// 	<div class=\"col-xs-5\">
+			// 		<img src=\"img/map-img.jpg\">
+			// 	</div>
+			// 	<div class=\"col-xs-7\">
+			// 		<p><strong>Nombre: </strong> Lorem ipsum dolor</p>
+			// 		<p><strong>Dirección: </strong> Lorem ipsum dolor</p>
+			// 		<p><strong>Teléfono: </strong> (+571) 123 4567</p>
+			// 		<p><strong>Correo: </strong><a href=\"mailto:#\">info@email.com</a></p>
+			// 	</div>
+			// </div>
+		    // ";
+		    
+		    $models[] = $attr;
+		}
+		$typeRender='render';
+		if(Yii::app()->request->isAjaxRequest)
+			$typeRender='renderPartial';
+		
+		$this->{$typeRender}('index',array(
+			'models'=>$models,
+			'model'=>$model,
+		));
+	}
+
+}
---
+++ modules/gii/generators/crud/templates/cms-map/index.php
@@ -0,0 +1,312 @@
+<?php
+/**
+ * The following variables are available in this template:
+ * - $this: the CrudCode object
+ */
+$module=Yii::app()->getModule('gii');
+?>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+<script>
+//Funciones Mapa
+
+/*-------------------------INICIALIZACIÓN DEL MAPA---------------------------*/
+var map;
+var polygons = [];
+var markers = [];
+var infowindow = new google.maps.InfoWindow({
+    content: ''
+});
+
+function initialize() {
+
+
+    var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(4.659634, -74.062035),
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'usroadatlas']
+        },
+        // panControl: false,
+        // zoomControl: false
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    /*ESTILOS DEL MAPA*/
+    var mapPeepStyle = [{
+            stylers: [{
+                    lightness: 1
+                }, {
+                    saturation: -48
+                }, {
+                    hue: "#005eff"
+                }]
+        }];
+
+    var styledMapOptions = {
+        name: '<?=r()->name?> Map'
+    };
+
+    var colMapStyle = new google.maps.StyledMapType(mapPeepStyle, styledMapOptions);
+
+    map.mapTypes.set('usroadatlas', colMapStyle);
+    map.setMapTypeId('usroadatlas');
+
+    /*MARKERS*/
+    var markersCollection = <?php echo "<?php echo CJSON::encode(\$models)?>"?>;
+
+    /*POLIGONOS*/
+    // var polygonsCollection = {};
+    
+    // addMarkerColor(new google.maps.LatLng(4.663056, -74.066713), "ccc");
+    createMarkers(markersCollection, map);
+    // createPolygons(polygonsCollection, map);
+}
+
+/*FUNCIONES CON POLUGONOS*/
+function createPolygons(polygonsCollection, parentMap) {
+    if (polygonsCollection.length > 0) {
+    	var color = '#46D6AC';
+
+        for (var i = 0; i < polygonsCollection.length; i++) {
+
+            var polygonCoords = [];
+
+        	var color = polygonsCollection[i][0].color;
+        	var onePoint = new google.maps.LatLng(polygonsCollection[i][0].lat, polygonsCollection[i][0].lng);
+            for (var j = 0; j < polygonsCollection[i].length; j++) {
+                var polygonCoord = new google.maps.LatLng(polygonsCollection[i][j].lat, polygonsCollection[i][j].lng);
+                polygonCoords.push(polygonCoord);
+			}
+            ;
+
+            var myPolygon = new google.maps.Polygon({
+                paths: polygonCoords,
+                strokeColor: color,
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: color,
+                fillOpacity: 0.35,
+                map: parentMap
+            });
+
+            // google.maps.event.addListener(myPolygon, 'click', function(polygon) {
+            //     console.log(polygon);
+            // });
+
+			(function(polygon, contenido, onePoint){                       
+			    google.maps.event.addListener(polygon, 'click', function() {
+			        infowindow.setContent(contenido);
+			        infowindow.open(map);
+			        infowindow.setPosition(onePoint);
+			    });
+			})(myPolygon,polygonsCollection[i][0].content_event,onePoint);
+
+
+            polygons.push(myPolygon);
+        }
+    }
+}
+
+
+// Sets the map on all polygons in the array.
+function setAllPolygonsMap(map) {
+    for (var i = 0; i < polygons.length; i++) {
+        polygons[i].setMap(map);
+    }
+}
+
+// Removes the polygons from the map, but keeps them in the array.
+function clearPolygons() {
+    setAllPolygonsMap(null);
+}
+
+// Shows any polygons currently in the array.
+function showPolygons() {
+    setAllPolygonsMap(map);
+}
+
+// Deletes all polygons in the array by removing references to them.
+function deletePolygons() {
+    clearPolygons();
+    polygons = [];
+}
+
+/*FUNCIONES CON MARKERS*/
+function createMarkers(markersCollection, parentMap) {
+    if (markersCollection.length > 0) {
+
+        for (var i = 0; i < markersCollection.length; i++) {
+
+            var markerCoords = new google.maps.LatLng(markersCollection[i].lat, markersCollection[i].lng);
+
+            var pinColor = markersCollection[i].color;
+			var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+			    new google.maps.Size(21, 34),
+			    new google.maps.Point(0,0),
+			    new google.maps.Point(10, 34));
+			var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+			    new google.maps.Size(40, 37),
+			    new google.maps.Point(0, 0),
+			    new google.maps.Point(12, 35));
+	
+
+            var myMarker = new google.maps.Marker({
+                position: markerCoords,
+                draggable: false,
+                icon: pinImage,
+	            shadow: pinShadow,
+                // icon: '<?php echo Yii::app()->request->baseUrl?>/img/etiqueta.png',
+                map: map
+            });
+
+            myMarker.setMap(parentMap);
+
+			(function(marker, contenido){                       
+			    google.maps.event.addListener(marker, 'click', function() {
+			        infowindow.setContent(contenido);
+			        infowindow.open(map, marker);
+			    });
+			})(myMarker,markersCollection[i].content_event);
+
+            markers.push(myMarker);
+        }
+    }
+}
+
+// Add a marker to the map and push to the array.
+function addMarker(location) {
+    var marker = new google.maps.Marker({
+        position: location,
+        draggable: true,
+        title: 'Arrastrame',
+        icon: '<?php echo "<?php echo r()->request->baseUrl?>"?>/img/etiqueta.png',
+        map: map
+    });
+
+    markers.push(marker);
+}
+
+// Add a marker to the map and push to the array.
+function addMarkerColor(location, color) {
+
+    var pinColor = color;
+	var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+	    new google.maps.Size(21, 34),
+	    new google.maps.Point(0,0),
+	    new google.maps.Point(10, 34));
+	var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+	    new google.maps.Size(40, 37),
+	    new google.maps.Point(0, 0),
+	    new google.maps.Point(12, 35));
+	var marker = new google.maps.Marker({
+	            position: location, 
+	            map: map,
+	            icon: pinImage,
+	            shadow: pinShadow
+	        });
+	markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setAllMarkersMap(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    setAllMarkersMap(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+    setAllMarkersMap(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+$(function(){
+	$(document).on('submit','#search-form',function(){
+		$.ajax({
+			dataType: 'json',
+			data: $(this).serialize(),
+			success: function(data) {
+				deleteMarkers();
+				// deletePolygons();
+				createMarkers(data.models, map);
+				// createPolygons(data.pol, map);
+			},
+		});
+		return false;
+	});
+	$(document).on('click','#search-form-button',function(e){
+		e.preventDefault();
+		$.ajax({
+			dataType: 'json',
+			data: $('#search-form').serialize(),
+			success: function(data) {
+				deleteMarkers();
+				// deletePolygons();
+				createMarkers(data.models, map);
+				// createPolygons(data.pol, map);
+			},
+		});
+	});
+
+	$(document).on('click','#all',function(e) {
+		e.preventDefault();
+		$('#search-form').each(function(){
+			this.reset();
+		});
+		$.ajax({
+			 type:'GET',
+			 dataType: 'json',
+			 url:'<?php echo "<?=r()->createUrl(\$this->route)?>"?>',
+			 data:{},
+			 success: function(data) {
+				deleteMarkers();
+				// deletePolygons();
+				createMarkers(data.models, map);
+				// createPolygons(data.pol, map);
+			},
+		});
+	});
+})
+</script>
+<script>google.maps.event.addDomListener(window, 'load', initialize);</script>
+<section class="panel">
+    <div class="panel-body minimal">
+        <div class="table-inbox-wrap">
+<?php echo "<?php"; ?> $form=$this->beginWidget('CActiveForm',array(
+    'id'=>'search-form',
+	'action'=>r()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+		<div class="row">
+		<?php foreach($this->tableSchema->columns as $column):
+			$tangaColumn=$module->getParamsField($column);
+			if($tangaColumn['type']=='img' or $tangaColumn['type']=='file')
+				continue;
+
+			$columnLat=explode('_', $column->name);
+			if(isset($columnLat[0]) and isset($columnLat[2]) and $columnLat[0]=='map' and ($columnLat[2]=='lat' or $columnLat[2]=='lng'))
+				continue; ?>
+			<div class="col-lg-2">
+				<?php echo "<?php echo ".$this->generateActiveField($this->modelClass,$column)."; ?>\n"; ?>
+			</div>
+		<?php endforeach;?>
+			<div class="col-lg-2">
+				<?php echo "<?php echo CHtml::link('<i class=\"fa fa-search\"></i>','#',array('class'=>'btn btn-default','id'=>'search-form-button'))?>\n"?>
+				<?php echo "<?php echo CHtml::link('<i class=\"fa fa-refresh\"></i> '.r('app','Ver todos'),'#',array('class'=>'btn btn-default','id'=>'all'))?>\n"?>
+			</div>
+		</div>
+<?php echo "<?php \$this->endWidget(); ?>\n"?>
+		</div>
+    </div>
+</section>
+<div style="width:100%;height: 550px" id="map"></div>
