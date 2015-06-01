2232237a107edc19d266c1c4e2ff86f09c7fbda1
Gustavo Salgado <gustavo@apptitude.com.co>
2015-06-01 05:48:13 -0500

    Clean

--- modules/_landing/README.md
+++
@@ -1,4 +0,0 @@
-retalapp-module-landing
-=======================
-
-Main module for to show difffrent king of blocks on landing page
--- modules/_landing/views/page/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* @var $this BackController */
-
-$this->breadcrumbs=array(
-	$this->module->id,
-);
-
-?>
-<?=$this->module->getBody()?>
